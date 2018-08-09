31446ad4d2a788a56d888d19483841c8e0f7ed87
sameerahmed19921208nb1297795223056 <sameer129nb@gmail.com>
2018-08-09 17:59:38 +0530

    First C# mail code

---
+++ SendingMail/Index.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Index.aspx.cs" Inherits="Index" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:Panel ID="Panel1" runat="server" DefaultButton="btnSubmit">
+        <p>
+            Please Fill the Following to Send Mail.</p>
+        <p>
+            Your name:
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator11" runat="server" ErrorMessage="*"
+                ControlToValidate="YourName" ValidationGroup="save" /><br />
+            <asp:TextBox ID="YourName" runat="server" Width="250px" /><br />
+            Your email address:
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="*"
+                ControlToValidate="YourEmail" ValidationGroup="save" /><br />
+            <asp:TextBox ID="YourEmail" runat="server" Width="250px" Text="same6fine@gmail.com" />
+           <%-- <asp:RegularExpressionValidator runat="server" ID="RegularExpressionValidator23" 
+                SetFocusOnError="true" Text="Example: username@gmail.com" ControlToValidate="YourEmail"
+                ValidationExpression="\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" Display="Dynamic"
+                ValidationGroup="save" />--%><br />
+            Subject:
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="*"
+                ControlToValidate="YourSubject" ValidationGroup="save" /><br />
+            <asp:TextBox ID="YourSubject" runat="server" Width="400px" /><br />
+            Your Question:
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="*"
+                ControlToValidate="Comments" ValidationGroup="save" /><br />
+            <asp:TextBox ID="Comments" runat="server" TextMode="MultiLine" Rows="10" Width="400px" />
+        </p>
+        <p>
+            <asp:Button ID="btnSubmit" runat="server" Text="Send" OnClick="Button1_Click" ValidationGroup="save" />
+        </p>
+    </asp:Panel>
+    <p>
+        <asp:Label ID="DisplayMessage" runat="server" Visible="false" />
+    </p>
+    </form>
+</body>
+</html>
---
+++ SendingMail/Index.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+
+public partial class Index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void SendMail()
+    {
+        // Gmail Address from where you send the mail
+        var fromAddress = "same1fine@gmail.com";
+        // any address where the email will be sending
+        var toAddress = YourEmail.Text.ToString();
+        //Password of your gmail address
+        const string fromPassword = "9844800219";
+        // Passing the values and make a email formate to display
+        string subject = YourSubject.Text.ToString();
+        string body = "From: " + YourName.Text + "\n";
+        body += "Email: " + YourEmail.Text + "\n";
+        body += "Subject: " + YourSubject.Text + "\n";
+        body += "Question: \n" + Comments.Text + "\n";
+        body += "<img src='https://i.pinimg.com/736x/97/15/38/97153836f02c8dbed0d1135dabe4fcab--most-expensive-expensive-watches.jpg'>";
+        body += "<table>";
+        body += "<tr style=';background-color:cyan;'>";
+        body += "<td style=''>Sl.no</td>";
+        body += "<td style=''>Order Quantity</td>";
+        body += "<td style=''>Price</td>";
+        body += "</tr>";
+        body += "</table>";
+
+        // smtp settings
+        var smtp = new System.Net.Mail.SmtpClient();
+        {
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = true;
+            smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+            smtp.Timeout = 20000;
+
+        }
+        // Passing values to smtp object
+        var msg = new MailMessage(fromAddress, toAddress, subject, body);
+        msg.IsBodyHtml = true;
+        smtp.Send(msg);
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            //here on button click what will done 
+            SendMail();
+            DisplayMessage.Text = "Mail sent successfully";
+            DisplayMessage.Visible = true;
+            YourSubject.Text = "";
+            YourEmail.Text = "";
+            YourName.Text = "";
+            Comments.Text = "";
+        }
+        catch (Exception) { }
+    }
+
+
+}
